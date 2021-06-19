283e58491199e4d98e7ef2c8a1280420adcd006b
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-06-19 22:14:29 +0100

    Create Details Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Details from './components/Details/Details';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/details" component={Details} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
