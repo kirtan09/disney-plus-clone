eed05f3ad28c6020605971c3986a9835271880be
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-06-19 17:31:59 +0100

    Creating ImageSlider for Home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
   return (
     <Nav>
-      <Logo src='/images/logo.svg' />
+      <Logo src="/images/logo.svg" />
       <NavMenu>
         <a href="/#">
-          <img src='/images/home-icon.svg' alt='Icon'/>
+          <img src="/images/home-icon.svg" alt="Icon" />
           <span>HOME</span>
         </a>
         <a href="/#">
-          <img src='/images/search-icon.svg' alt='Icon'/>
+          <img src="/images/search-icon.svg" alt="Icon" />
           <span>SEARCH</span>
         </a>
         <a href="/#">
-          <img src='/images/watchlist-icon.svg' alt='Icon'/>
+          <img src="/images/watchlist-icon.svg" alt="Icon" />
           <span>WATCHLIST</span>
         </a>
         <a href="/#">
-          <img src='/images/original-icon.svg' alt='Icon'/>
+          <img src="/images/original-icon.svg" alt="Icon" />
           <span>ORIGINALS</span>
         </a>
         <a href="/#">
-          <img src='/images/movie-icon.svg' alt='Icon'/>
+          <img src="/images/movie-icon.svg" alt="Icon" />
           <span>MOVIES</span>
         </a>
         <a href="/#">
-          <img src='/images/series-icon.svg' alt='Icon'/>
+          <img src="/images/series-icon.svg" alt="Icon" />
           <span>SERIES</span>
         </a>
       </NavMenu>
-      <UserImg src='/images/movie-icon.svg' alt='Icon'/>
+      <UserImg src="/images/movie-icon.svg" alt="Icon" />
     </Nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
 
 const Nav = styled.div`
   height: 70px;
@@ -44,11 +44,12 @@ const Nav = styled.div`
   display: flex;
   align-items: center;
   padding: 0 30px;
-  `
+  overflow: hidden;
+`;
 
 const Logo = styled.img`
   width: 80px;
-`
+`;
 
 const NavMenu = styled.div`
   display: flex;
@@ -61,6 +62,8 @@ const NavMenu = styled.div`
     align-items: center;
     padding: 0 12px;
     cursor: pointer;
+    color: inherit;
+    text-decoration: inherit;
 
     img {
       height: 20px;
@@ -72,7 +75,7 @@ const NavMenu = styled.div`
       position: relative;
 
       &:after {
-        content: "";
+        content: '';
         height: 2px;
         background: white;
         position: absolute;
@@ -81,10 +84,11 @@ const NavMenu = styled.div`
         bottom: -6px;
         opacity: 0;
         transform: scaleX(0);
+        transform-origin: left;
         transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
       }
     }
-    
+
     &:hover {
       span:after {
         transform: scaleX(1);
@@ -92,10 +96,10 @@ const NavMenu = styled.div`
       }
     }
   }
-`
+`;
 
 const UserImg = styled.img`
   width: 48px;
   height: 48px;
   border-radius: 50%;
-`
+`;
